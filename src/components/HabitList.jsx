2a7e48b6cfac95f160b0f5966aab7ab8eb3aa940
Guilherme Vasques <mvasguilherme@gmail.com>
2024-07-31 13:45:12 -0300

    Primeiro Prototipo do Card

--- src/components/HabitList.jsx
+++ src/components/HabitList.jsx
@@ -4,11 +4,11 @@ const HabitList = ({ habits }) => {
   return (
     <div className="text-white">
       <div className="">
-        <ul>
+        <ul className='grid grid-cols-4 gap-4 mt-10 '>
           {habits.map((habit, index) => (
-            <li key={index}>
-              <h1>{habit.habitName}</h1>
-              <h3>{habit.frequency}</h3>
+            <li className=' transform duration-200 mr-3 mb-3 hover:scale-105 cursor-pointer bg-white rounded-md text-center'key={index}>
+              <h1 className='p-3 text-2xl text-secondary font-medium'>{habit.habitName}</h1>
+              <h3 className=' bg-green p-2 rounded-md  text-white'>{habit.frequency}</h3>
             </li>
           ))}
         </ul>
